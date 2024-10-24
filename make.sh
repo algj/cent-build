99a538d2473472dc253889752358524b4c0d9ab8
algj <33424336+algj@users.noreply.github.com>
2024-10-24 11:49:50 +0300

    latest make

---
+++ make.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+MAKE_VERSION=$(curl -s https://ftp.gnu.org/gnu/make/ | grep -oP 'make-\K[0-9]+\.[0-9]+' | sort -V | tail -n 1)
+MAKE_TAR="make-${MAKE_VERSION}.tar.gz"
+
+echo "Downloading GNU Make ${MAKE_VERSION}..."
+wget https://ftp.gnu.org/gnu/make/${MAKE_TAR}
+
+echo "Extracting GNU Make..."
+tar -xvf ${MAKE_TAR}
+
+cd "make-${MAKE_VERSION}"
+
+echo "Configuring and compiling GNU Make..."
+./configure
+make
+sudo make install
+
+make --version
+
+cd ..
+rm -rf "make-${MAKE_VERSION}" ${MAKE_TAR}
+
+echo "GNU Make ${MAKE_VERSION} installed successfully!"
