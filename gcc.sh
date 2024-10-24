4ac56292c767a087ff96c34bca6c73555ee62fde
algj <33424336+algj@users.noreply.github.com>
2024-10-25 00:08:21 +0300

    gcc

--- gcc.sh
+++ gcc.sh
@@ -3,18 +3,17 @@
 set -e
 
 #GCC_VERSION=$(curl -s https://ftp.gnu.org/gnu/gcc/ | grep -oP 'gcc-\K[0-9]+\.[0-9]+\.[0-9]+' | sort -V | tail -n 1)
-GCC_VERSION="5.5.0"
-GCC_URL="https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.gz"
+GCC_VERSION="5.4.0"
+GCC_URL="https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.bz2"
 INSTALL_DIR="/usr/local"
 
 sudo yum groupinstall -y "Development Tools"
 sudo yum install -y gmp gmp-devel mpfr mpfr-devel libmpc libmpc-devel wget curl bison flex texinfo
 
-
 cd /usr/local/src
 wget $GCC_URL
 
-tar -xzf "gcc-$GCC_VERSION.tar.gz"
+tar -xjf "gcc-$GCC_VERSION.tar.bz2"
 cd "gcc-$GCC_VERSION"
 
 ./contrib/download_prerequisites
@@ -23,7 +22,6 @@ mkdir build
 cd build
 
 ../configure --enable-languages=c,c++ --disable-multilib --prefix=$INSTALL_DIR
-
 make -j$(nproc)
 sudo make install
 
@@ -31,6 +29,6 @@ sudo ldconfig
 
 cd /usr/local/src
 rm -rf "gcc-$GCC_VERSION"
-rm "gcc-$GCC_VERSION.tar.gz"
+rm "gcc-$GCC_VERSION.tar.bz2"
 
 echo "GCC $GCC_VERSION has been installed successfully!"
