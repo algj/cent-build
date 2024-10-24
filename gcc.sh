a743722b9ba393b6a7f69ab32167314f7684f918
algj <33424336+algj@users.noreply.github.com>
2024-10-25 01:41:03 +0300

    bz2 to gz

--- gcc.sh
+++ gcc.sh
@@ -3,7 +3,7 @@
 set -e
 
 GCC_VERSION=$(curl -s https://ftp.gnu.org/gnu/gcc/ | grep -oP 'gcc-\K[0-9]+\.[0-9]+\.[0-9]+' | sort -V | tail -n 1)
-GCC_URL="https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.bz2"
+GCC_URL="https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.gz"
 INSTALL_DIR="/usr/local"
 
 sudo yum groupinstall -y "Development Tools"
@@ -12,7 +12,7 @@ sudo yum install -y gmp gmp-devel mpfr mpfr-devel libmpc libmpc-devel wget curl 
 cd /usr/local/src
 wget $GCC_URL
 
-tar -xjf "gcc-$GCC_VERSION.tar.bz2"
+tar -xvf "gcc-$GCC_VERSION.tar.gz"
 cd "gcc-$GCC_VERSION"
 
 ./contrib/download_prerequisites
@@ -28,6 +28,6 @@ sudo ldconfig
 
 cd /usr/local/src
 rm -rf "gcc-$GCC_VERSION"
-rm "gcc-$GCC_VERSION.tar.bz2"
+rm "gcc-$GCC_VERSION.tar.gz"
 
 echo "GCC $GCC_VERSION has been installed successfully!"
